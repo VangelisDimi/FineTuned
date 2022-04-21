f340bd85d8c3a7437595a822762a1158ab9f67e2
VangelisDimi <evangelos.dhmh@gmail.com>
2022-04-21 18:57:17 +0300

    gui changes

--- gui.py
+++ gui.py
@@ -1,13 +1,45 @@
 import tkinter as tk
-import os
+from tkinter import font
 
-#Window
-app = tk.Tk()
-app.title("PyTuner")
-winimg = tk.PhotoImage(file='./Assets/icon_128.png')
-app.tk.call('wm', 'iconphoto', app._w, winimg)
+from audio_read import read_real_time_audio
+from audio_utils import audio_fft, frequency_to_note
 
-width = app.winfo_screenwidth()
-height = app.winfo_screenheight()
+import pyglet
+pyglet.font.add_file('Assets/LcdSolid-VPzB.ttf')   
 
-app.mainloop()
+class main_window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("PyTuner")
+        self.geometry("400x400")
+
+        #Note Display
+        self.Note_label = tk.Label(self,text='',font=("LCD Solid", 50 , 'bold'))
+        self.Note_label.pack()
+    
+    def clear_labels(self):
+        # remove text from all labels
+        self.Note_label['text'] = ""
+    
+    def update_labels(self,Note):
+        # update all labels
+        self.Note_label.configure(text=Note)
+        self.after(2000, self.clear_labels)
+
+def main_gui():
+    app = main_window()
+    app.mainloop()
+
+    audio_generator = read_real_time_audio()
+
+    for sample_rate, signal in audio_generator:
+        _, _, _, loudest_frequency, loudest_frequency_amplitude = audio_fft(signal, sample_rate)
+        closest_frequency, closest_note = frequency_to_note(loudest_frequency, loudest_frequency_amplitude)
+        tune_direction = None
+
+        if closest_frequency is not None and closest_note is not None:
+            app.update_labels(closest_note)
+        app.update()
+    
+    audio_generator.close()
