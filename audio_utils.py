07b50ceec0b451f74b8663ca696340181bc97050
d-maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2022-05-18 19:08:26 +0300

    replaced hard-coded note frequencies with mathematic formula

--- audio_utils.py
+++ audio_utils.py
@@ -24,32 +24,33 @@ def audio_fft(signal, sample_rate, samples=None):
     return frequencies, xf, yf, loudest_frequency, frequencies[loudest_frequency]
 
 
-# TODO: find a more efficient way to calculate closest frequency
-# TODO: support more tunings (not only 440 Hz)
-# TODO: find octave of closest note
-def frequency_to_note(input_frequency, input_frequency_amplitude):
+def frequency_to_note(input_frequency, input_frequency_amplitude, f_0=440.0):
     """
+    Returns the closest note and it's frequency, given an input frequency
 
     :param input_frequency: the loudest input frequency (fundamental)
     :param input_frequency_amplitude: the amplitude of the input frequency
+    :param f_0: the frequency of A4 note (default: 440.0)
     :return: a tuple with closest note frequency and closest note name
     """
 
     # source: http://techlib.com/reference/musical_note_frequencies.htm
-    note_frequencies = {
-        'A': [55.0, 110.0, 220.0, 440.0, 880.0],
-        'A#': [58.27, 116.54, 233.08, 466.16, 932.32],
-        'B': [61.74, 123.48, 246.96, 493.92, 987.84],
-        'C': [65.41, 130.82, 261.64, 523.28, 1046.56],
-        'C#': [69.30, 138.60, 277.20, 554.40, 1108.80],
-        'D': [73.42, 146.84, 293.68, 587.36, 1174.72],
-        'D#': [77.78, 155.56, 311.12, 622.24, 1244.48],
-        'E': [82.41, 164.82, 329.64, 659.28, 1318.56],
-        'F': [87.31, 174.62, 349.24, 698.48, 1396.96],
-        'F#': [92.50, 185.00, 370.00, 740.00, 1480.00],
-        'G': [98.00, 196.00, 392.00, 784.00, 1568.00],
-        'Ab': [103.83, 207.66, 415.32, 830.64, 1661.28]
-    }
+    # note_frequencies = {
+    #     'A': [55.0, 110.0, 220.0, 440.0, 880.0],
+    #     'A#': [58.27, 116.54, 233.08, 466.16, 932.32],
+    #     'B': [61.74, 123.48, 246.96, 493.92, 987.84],
+    #     'C': [65.41, 130.82, 261.64, 523.28, 1046.56],
+    #     'C#': [69.30, 138.60, 277.20, 554.40, 1108.80],
+    #     'D': [73.42, 146.84, 293.68, 587.36, 1174.72],
+    #     'D#': [77.78, 155.56, 311.12, 622.24, 1244.48],
+    #     'E': [82.41, 164.82, 329.64, 659.28, 1318.56],
+    #     'F': [87.31, 174.62, 349.24, 698.48, 1396.96],
+    #     'F#': [92.50, 185.00, 370.00, 740.00, 1480.00],
+    #     'G': [98.00, 196.00, 392.00, 784.00, 1568.00],
+    #     'G#': [103.83, 207.66, 415.32, 830.64, 1661.28]
+    # }
+
+    notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
     frequency_threshold = 15
     amplitude_threshold = 1000000  # TODO: play with threshold on different devices
@@ -62,26 +63,38 @@ def frequency_to_note(input_frequency, input_frequency_amplitude):
 
     closest_frequency = None
     closest_note = None
-    for note in note_frequencies.keys():
-        for frequency in note_frequencies[note]:
-            if abs(input_frequency - frequency) < frequency_threshold and (closest_frequency is None or (
-                    abs(input_frequency - frequency) < abs(input_frequency - closest_frequency))):
-                closest_frequency = frequency
-                closest_note = note
+
+    a = 2 ** (1 / 12)
+    for n in range(-37, 23):
+        frequency = f_0 * a ** (n + 1)  # source: https://pages.mtu.edu/~suits/NoteFreqCalcs.html
+
+        if closest_frequency is not None and abs(frequency - input_frequency) > closest_frequency:
+            break
+
+        if abs(input_frequency - frequency) < frequency_threshold and (closest_frequency is None or (
+                abs(input_frequency - frequency) < abs(input_frequency - closest_frequency))):
+            closest_frequency = frequency
+            closest_note = notes[(n + 1) % 12]
 
     return closest_frequency, closest_note
 
 
-def neighbour_note_frequency(note_frequency, frequency):
-    f_o = 440.0
+def neighbour_note_frequency(note_frequency, frequency, f_0=440.0):
+    """
+
+    :param note_frequency: the closest note frequency
+    :param frequency: the input
+    :param f_0: the frequency of A4 note (default: 440.0)
+    :return: returns the frequency of the second closest note
+    """
     a = 2 ** (1 / 12)
 
-    n = math.log(note_frequency / f_o, a)
+    n = math.log(note_frequency / f_0, a)
 
     if frequency > note_frequency:
-        return f_o * a ** (n + 1)
+        return f_0 * a ** (n + 1)
     elif frequency < note_frequency:
-        return f_o * a ** (n - 1)
+        return f_0 * a ** (n - 1)
 
     return None
 
